11a30f880f11155feb254b544a240fdf81fd13cf
aakashpugazh <aakashpugazh@gmail.com>
2022-11-20 15:32:59 +0530

    Final Deliveries

--- Final Deliveries/Final Code/sendemail.py
+++ Final Deliveries/Final Code/sendemail.py
@@ -7,7 +7,7 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 def sendmail(TEXT,email):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("akashkumar992002@gmail.com", "spwoaftjqihfduhf")
+    s.login("akashkumar992002@gmail.com", "******")
     message  = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
     s.sendmail("akashkumar992002@gmail.com", email, message)
     s.quit()
